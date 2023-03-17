0dc2667451a560c1021f7e3f23ac371afa36c873
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-03-17 13:56:48 +0100

    added projects images, small fixes on design

--- source/main.js
+++ source/main.js
@@ -1,14 +1,16 @@
-var icon = document.getElementById("icon");
-var icon1 = document.getElementById("a");
-var icon2 = document.getElementById("b");
-var icon3 = document.getElementById("c");
-var nav = document.getElementById('nav');
-var blue = document.getElementById("blue");
+const icon = document.getElementById("icon");
+const icon1 = document.getElementById("a");
+const icon2 = document.getElementById("b");
+const icon3 = document.getElementById("c");
+const nav = document.getElementById('nav');
 
 icon.addEventListener('click', function() {
   icon1.classList.toggle('a');
   icon2.classList.toggle('c');
   icon3.classList.toggle('b');
   nav.classList.toggle('show');
-  blue.classList.toggle('slide');
 });
+
+let encEmail = "c3V2aS5wcmljZWxsYUBnbWFpbC5jb20=";
+const form = document.querySelector(".fa-envelope");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
