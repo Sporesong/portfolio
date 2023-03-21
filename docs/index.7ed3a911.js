6f43280121165d0a14e0498650a823df8d1640d9
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-03-21 19:09:20 +0100

    New build test

---
+++ docs/index.7ed3a911.js
@@ -0,0 +1,2 @@
+const icon=document.getElementById("icon"),icon1=document.getElementById("a"),icon2=document.getElementById("b"),icon3=document.getElementById("c"),nav=document.getElementById("nav");icon.addEventListener("click",(function(){icon1.classList.toggle("a"),icon2.classList.toggle("c"),icon3.classList.toggle("b"),nav.classList.toggle("show")}));let encodedEmail="c3V2aS5wcmljZWxsYUBnbWFpbC5jb20=";const emailLink=document.getElementById("contact");emailLink.setAttribute("href","mailto:".concat(atob(encodedEmail)));
+//# sourceMappingURL=index.7ed3a911.js.map
